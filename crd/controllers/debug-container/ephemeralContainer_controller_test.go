4fe10903a445fc6a4e7655c6f3f4f0f55f71d018
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2023-02-22 15:05:14 +0200

    Improve testing

--- crd/controllers/debug-container/ephemeralContainer_controller_test.go
+++ crd/controllers/debug-container/ephemeralContainer_controller_test.go
@@ -5,6 +5,8 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
 )
 
@@ -35,3 +37,42 @@ var _ = Describe("ephemeralContainerExists", func() {
 		Expect(ephemeralContainerExists(pod)).To(BeTrue())
 	})
 })
+
+func TestGenerateDebugContainers(t *testing.T) {
+	// GIVEN
+	pod := v1.Pod{}
+	privileged := true
+	expected_pod := v1.Pod{
+		Spec: v1.PodSpec{
+			EphemeralContainers: []v1.EphemeralContainer{
+				{
+					EphemeralContainerCommon: v1.EphemeralContainerCommon{
+						Name:                     "debugger",
+						Image:                    "instrumentisto/nmap:latest",
+						ImagePullPolicy:          v1.PullIfNotPresent,
+						Stdin:                    true,
+						TerminationMessagePolicy: v1.TerminationMessageReadFile,
+						TTY:                      true,
+						Command:                  []string{"sh"},
+						SecurityContext: &v1.SecurityContext{
+							Privileged: &privileged,
+						},
+					},
+				},
+				{
+					EphemeralContainerCommon: v1.EphemeralContainerCommon{
+						Name:                     "monitor",
+						Image:                    "registry.cs.aalto.fi/kubesonde/monitor:latest",
+						ImagePullPolicy:          v1.PullIfNotPresent,
+						Stdin:                    true,
+						TerminationMessagePolicy: v1.TerminationMessageReadFile,
+						TTY:                      true,
+						Command:                  []string{"sh"},
+					},
+				},
+			},
+		},
+	}
+	result := lo.Must1(generateDebugContainers(&pod))
+	assert.Equal(t, &expected_pod, result)
+}
--- crd/go.mod
+++ crd/go.mod
@@ -69,6 +69,7 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gotest.tools v2.2.0+incompatible
 	k8s.io/apiextensions-apiserver v0.24.2 // indirect
 	k8s.io/component-base v0.24.2 // indirect
 	k8s.io/klog/v2 v2.70.0 // indirect
