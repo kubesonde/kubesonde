47e06bd6e693c375946d095760d83a9bdec7187b
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2023-02-22 15:31:08 +0200

    Update deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,8 +3,8 @@ set -x
 VERSION=$(npm version patch)
 git add .
 echo "Bump version to ${VERSION}" | git commit -F -
-git tag ${VERSION}
-git push origin ${VERSION}
+git tag "${VERSION}"
+git push origin "${VERSION}"
 git push origin dev
 git checkout main
 git merge origin/dev
