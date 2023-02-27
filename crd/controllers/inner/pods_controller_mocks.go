8589e4075f079fc7882eddb73c12b21c61b1f22d
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2023-02-27 13:16:14 +0200

    Remove unused functions

--- crd/controllers/inner/pods_controller_mocks.go
+++ crd/controllers/inner/pods_controller_mocks.go
@@ -14,14 +14,6 @@ func (mock *MockedCNIState) getClient() kubernetes.Interface {
 	args := mock.Called()
 	return args.Get(0).(kubernetes.Interface)
 }
-func (mock *MockedCNIState) logError(err error, message string) {
-	mock.Called(err, message)
-
-}
-func (mock *MockedCNIState) logInfo(value string) {
-	mock.Called(value)
-
-}
 
 func (mock *MockedCNIState) runCommand(client kubernetes.Interface, namespace string, command probe_command.KubesondeCommand, checker func(string) bool) (bool, error) {
 	ret := mock.Called(client, namespace, command, checker)
@@ -32,10 +24,5 @@ func (mock *MockedCNIState) runCommand(client kubernetes.Interface, namespace st
 func (mock *MockedCNIState) runGenericCommand(client kubernetes.Interface, namespace string, command probe_command.KubesondeCommand) (string, error) {
 	ret := mock.Called(client, namespace, command)
 	return ret.Get(0).(string), ret.Error(1)
-
-}
-
-func (mock *MockedCNIState) AddEdge(_ probe_command.KubesondeCommand, _ bool) {
-	mock.Called()
 
 }
