b5a45bf8c47389f5dfc11d2267fd3f6fa4e637ab
Jacopo Bufalino <17317134+jackap@users.noreply.github.com>
2026-04-20 12:14:37 +0200

    add  function to comapre probe commands

--- crd/controllers/probe_command/probe.command.go
+++ crd/controllers/probe_command/probe.command.go
@@ -21,3 +21,40 @@ type KubesondeCommand struct {
 	SourceType           v1.ProbeEndpointType `json:"sourceType"`
 	SourceLabels         string               `json:"sourceLabels"`
 }
+
+type ComparableKubesondeCommand struct {
+	Command              string               `json:"command"`
+	ContainerName        string               `json:"ContainerName"`
+	Namespace            string               `json:"sourceNamespace"`
+	Protocol             string               `json:"protocol"`
+	Destination          string               `json:"destination"`
+	DestinationIPAddress string               `json:"destinationIPAddress"`
+	DestinationNamespace string               `json:"destinationNamespace"`
+	DestinationPort      string               `json:"destinationPort"`
+	DestinationLabels    string               `json:"destinationLabels"`
+	DestinationType      v1.ProbeEndpointType `json:"destinationType"`
+	SourcePodName        string               `json:"source"`
+	SourceIPAddress      string               `json:"sourceIPAddress"`
+	SourceType           v1.ProbeEndpointType `json:"sourceType"`
+	SourceLabels         string               `json:"sourceLabels"`
+}
+
+func (item KubesondeCommand) ToComparableCommand() ComparableKubesondeCommand {
+	return ComparableKubesondeCommand{
+		Command:              item.Command,
+		ContainerName:        item.ContainerName,
+		Namespace:            item.Namespace,
+		Protocol:             item.Protocol,
+		Destination:          item.Destination,
+		DestinationIPAddress: item.DestinationIPAddress,
+		DestinationNamespace: item.DestinationNamespace,
+		DestinationPort:      item.DestinationPort,
+		DestinationLabels:    item.DestinationLabels,
+		DestinationType:      item.DestinationType,
+		SourcePodName:        item.SourcePodName,
+		SourceIPAddress:      item.SourceIPAddress,
+		SourceType:           item.SourceType,
+		SourceLabels:         item.SourceLabels,
+	}
+
+}
